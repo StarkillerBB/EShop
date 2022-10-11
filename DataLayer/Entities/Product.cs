a64194f57241c374929be92e4aa5da5f5edf3822
Benjamin Marius Lank <benj007d@elevcampus.dk>
2022-10-11 15:04:06 +0200

    Add project files.

---
+++ DataLayer/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    internal class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; }
+        public int TypeID { get; set; }
+    }
+}
